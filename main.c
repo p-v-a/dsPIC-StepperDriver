5fe4e35f5a19de1891c5e66f7d749b9e8e2b71e0
Vladimir Pasashnikov <vladimir.pasashniko@gmail.com>
2018-02-16 14:07:32 +1100

    PID initial coefficient adjustments

--- main.c
+++ main.c
@@ -53,12 +53,12 @@ int main(void) {
  ******************************************************************************/
 void InitPid () {
     pid_parameters.kp = _Q16ftoi (10.0);
-    pid_parameters.ki = _Q16ftoi (10);
-    pid_parameters.kd = _Q16ftoi (2);
-    pid_parameters.kt = _Q16ftoi (0.0);
+    pid_parameters.ki = _Q16ftoi (3);
+    pid_parameters.kd = _Q16ftoi (10);
+    pid_parameters.kt = _Q16ftoi (3.0);
     
-    pid_parameters.wp = _Q16ftoi (0.5);
-    pid_parameters.wd = _Q16ftoi (0.5);
+    pid_parameters.wp = _Q16ftoi (1.0);
+    pid_parameters.wd = _Q16ftoi (1.0);
     
     pid_parameters.n = 8;
     
@@ -96,8 +96,8 @@ void MainControlLoop () {
                 
                 UpdateStepperState ();
 
-                UpdatePwm (&winding1, &power_supply_state, _Q16ftoi(100));
-                UpdatePwm (&winding2, &power_supply_state, _Q16ftoi(100));
+                UpdatePwm (&winding1, &power_supply_state, _Q16ftoi(0));
+                UpdatePwm (&winding2, &power_supply_state, _Q16ftoi(0));
                 
                 SetPwm();
                 
@@ -173,6 +173,16 @@ void UpdateStepperState () {
     IEC0bits.AD1IE = 1;
 }
 
+typedef struct {
+//    _Q16 target_current;
+    _Q16 measured_current;
+    CurrentDirection current_direction;
+    _Q16 adc_value;
+} PIDSample;
+#define SAMPLE_TABLE_SIZE 32
+
+PIDSample samples[SAMPLE_TABLE_SIZE];
+
 /******************************************************************************* 
  * Function:        ClosedLoopADCInterruptRoutine ()
  * Output:          None
@@ -181,6 +191,7 @@ void UpdateStepperState () {
  ******************************************************************************/
 inline 
 void ClosedLoopADCInterruptRoutine () {
+    static int sample_count = 0;
     static int alternatePID = 0;
     alternatePID = !alternatePID;
     // Read real-time DC voltage
@@ -188,9 +199,14 @@ void ClosedLoopADCInterruptRoutine () {
 
     // update PID parameters for winding 1
     if (alternatePID) {
+        if(sample_count > SAMPLE_TABLE_SIZE) {
+            sample_count = 0;
+        } 
+
         // 32768 is 0.5 in Q16. this is due to reference of the current amplifier, which is AVdd/2
         winding1.measured_current = (unsigned int)ADC1BUF1 - 32768;
-
+        samples[sample_count].adc_value = winding1.measured_current;
+        samples[sample_count].current_direction = winding1.current_direction;
         // Change current sign, if reverse current direction commanded 
         if (winding1.current_direction == Reverse)
             winding1.measured_current = _Q16neg(winding1.measured_current);
@@ -199,6 +215,12 @@ void ClosedLoopADCInterruptRoutine () {
         
         UpdatePwm (&winding1, &power_supply_state, winding1_pid_state.u0);
                 
+        samples[sample_count].measured_current = winding1.measured_current;
+//        samples[sample_count].target_current = winding1.target_current;
+        //samples[sample_count].pwm_value = winding1.desired_pwm;
+        //samples[sample_count].controller_output = winding1_pid_state.u0;
+        
+        ++sample_count;
         SetPwm();
 
         P1SECMPbits.SEVTCMP = (winding2.desired_pwm / 2 - 1);// - PWM_ACQ_DELAY; //PWM_FCY - PWM_ACQ_DELAY;
